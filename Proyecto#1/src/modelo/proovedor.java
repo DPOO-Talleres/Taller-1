419b0bfab8070b748a2b7d64805efaa94e87e0e1
AndresVilla203 <69772391+AndresVilla203@users.noreply.github.com>
2021-10-15 19:36:09 -0500

    Update proovedor.java

--- Proyecto#1/src/modelo/proovedor.java
+++ Proyecto#1/src/modelo/proovedor.java
@@ -1,5 +1,16 @@
 package modelo;
 
-public class proovedor {
+import java.util.Date;
+import java.util.HashMap;
 
+public class proovedor {
+	
+	private String Orden="";
+	
+	private HashMap<String, Producto > productos = new HashMap<>();
+	
+	private int precioTotalDeLaOrden=0;
+	
+	private Date fechaDeEntrega= new Date();
+	
 }
