2aacf489829a3d7c93a690867a308e1129ca5286
AndresVilla203 <69772391+AndresVilla203@users.noreply.github.com>
2021-10-15 19:35:13 -0500

    Update Cliente.java

--- Proyecto#1/src/modelo/Cliente.java
+++ Proyecto#1/src/modelo/Cliente.java
@@ -2,4 +2,13 @@ package modelo;
 
 public class Cliente {
 
+	private String Orden="";
+	
+	private String Orden="";
+	
+	private Boolean Orden="";
+	
+	private int Orden="";
+	
+	private String Orden="";
 }
